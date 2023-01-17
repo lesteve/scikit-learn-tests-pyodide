7adc70ea11ef6865175665746365044c2c383197
Loïc Estève <loic.esteve@ymail.com>
2023-01-17 14:15:35 +0100

    cleanup

--- scikit-learn-pytest.js
+++ scikit-learn-pytest.js
@@ -1,81 +1,12 @@
 const { opendir } = require('node:fs/promises');
 const { loadPyodide } = require("pyodide");
 
-// From https://github.com/numpy/numpy/pull/21895
-function make_tty_ops(stream){
-  return {
-    // get_char has 3 particular return values:
-    // a.) the next character represented as an integer
-    // b.) undefined to signal that no data is currently available
-    // c.) null to signal an EOF
-    get_char(tty) {
-      if (!tty.input.length) {
-        var result = null;
-        var BUFSIZE = 256;
-        var buf = Buffer.alloc(BUFSIZE);
-        var bytesRead = fs.readSync(process.stdin.fd, buf, 0, BUFSIZE, -1);
-        if (bytesRead === 0) {
-          return null;
-        }
-        result = buf.slice(0, bytesRead);
-        tty.input = Array.from(result);
-      }
-      return tty.input.shift();
-    },
-    put_char(tty, val) {
-      try {
-        if(val !== null){
-          tty.output.push(val);
-        }
-        if (val === null || val === 10) {
-          process.stdout.write(Buffer.from(tty.output));
-          tty.output = [];
-        }
-      } catch(e){
-        console.warn(e);
-      }
-    },
-    flush(tty) {
-      if (!tty.output || tty.output.length === 0) {
-        return;
-      }
-      stream.write(Buffer.from(tty.output));
-      tty.output = [];
-    }
-  };
-}
-
-// From https://github.com/numpy/numpy/pull/21895
-function setupStreams(FS, TTY){
-  let mytty = FS.makedev(FS.createDevice.major++, 0);
-  let myttyerr = FS.makedev(FS.createDevice.major++, 0);
-  TTY.register(mytty, make_tty_ops(process.stdout))
-  TTY.register(myttyerr, make_tty_ops(process.stderr))
-  FS.mkdev('/dev/mytty', mytty);
-  FS.mkdev('/dev/myttyerr', myttyerr);
-  FS.unlink('/dev/stdin');
-  FS.unlink('/dev/stdout');
-  FS.unlink('/dev/stderr');
-  FS.symlink('/dev/mytty', '/dev/stdin');
-  FS.symlink('/dev/mytty', '/dev/stdout');
-  FS.symlink('/dev/myttyerr', '/dev/stderr');
-  FS.closeStream(0);
-  FS.closeStream(1);
-  FS.closeStream(2);
-  FS.open('/dev/stdin', 0);
-  FS.open('/dev/stdout', 1);
-  FS.open('/dev/stderr', 1);
-}
-
 async function main() {
   let exit_code = 0;
   try {
     global.pyodide = await loadPyodide();
     let pyodide = global.pyodide;
     const FS = pyodide.FS;
-    // For now disabling this to avoid causing unknown issues. The pytest
-    // output is not as nice but we can live with this
-    // setupStreams(FS, pyodide._module.TTY);
     const NODEFS = FS.filesystems.NODEFS;
 
     let mountDir = "/mnt";
